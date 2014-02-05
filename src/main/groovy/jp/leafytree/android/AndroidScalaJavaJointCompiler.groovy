12f2e1f93b33004e27626847a16bdbeaff453bbd
Isamu Mogi <saturday6c@gmail.com>
2014-02-06 00:35:40 +0900

    Initial scribble implementation

---
+++ src/main/groovy/jp/leafytree/android/AndroidScalaJavaJointCompiler.groovy
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.leafytree.android
+
+import org.gradle.api.GradleException
+import org.gradle.api.Project
+import org.gradle.api.file.FileCollection
+import org.gradle.api.internal.tasks.compile.Compiler
+import org.gradle.api.internal.tasks.compile.JavaCompileSpec
+import org.gradle.api.tasks.WorkResult
+
+class AndroidScalaJavaJointCompiler implements Compiler<JavaCompileSpec> {
+    private final Project project
+    private final Compiler<JavaCompileSpec> androidJavaCompiler
+    private final Boolean test
+    private final Map extraOptions
+
+    public AndroidScalaJavaJointCompiler(Project project, Compiler<JavaCompileSpec> androidJavaCompiler, Boolean test, Map extraOptions) {
+        this.project = project
+        this.androidJavaCompiler = androidJavaCompiler
+        this.test = test
+        this.extraOptions = extraOptions
+    }
+
+    public WorkResult execute(JavaCompileSpec spec) {
+        // scala
+        def scalacDestinationDir = new File(spec.destinationDir.getAbsolutePath() + "-scala")
+        if (!scalacDestinationDir.exists() && !scalacDestinationDir.mkdirs()) {
+            throw new GradleException("Failed to create directory: $scalacDestinationDir")
+        }
+        def options = spec.compileOptions.optionMap().findAll {
+            [
+                    "srcdir", "classpath", "sourcepath", "bootclasspath",
+                    "extdirs", "target", "deprecation", "encoding", "failonerror",
+            ].contains(it.key)
+        }
+        if (!options["bootclasspath"] && spec.compileOptions.bootClasspath) {
+            options["bootclasspath"] = spec.compileOptions.bootClasspath
+        }
+        options += extraOptions
+        def ant = project.ant
+        ant.taskdef(name: 'scalac', classname: 'scala.tools.ant.Scalac', // TODO: import antlib xml
+                classpath: project.configurations.scalaCompileProvided.asPath)
+        options["destDir"] = scalacDestinationDir
+        ant.scalac(options) {
+            project.configurations.scalaCompileProvided.each { file ->
+                bootclasspath(location: file)
+            }
+            if (test) {
+                def parentDir = scalacDestinationDir.parentFile
+                if (parentDir.name != "test") {
+                    throw new GradleException("Unexpected directory structure: " + scalacDestinationDir.absolutePath)
+                }
+                def scalaMainClasspath = new File(parentDir.parentFile, scalacDestinationDir.name)
+                bootclasspath(location: scalaMainClasspath)
+            }
+            spec.source.addToAntBuilder(ant, 'src', FileCollection.AntType.MatchingTask)
+        }
+
+        // java
+        spec.source = spec.source.filter { !it.absolutePath.endsWith(".scala") }
+        if (spec.source.empty) {
+            return { true } as WorkResult
+        }
+        spec.classpath = spec.classpath + project.files(scalacDestinationDir)
+        def result = androidJavaCompiler.execute(spec)
+
+        // restore compiled scala classes
+        project.ant.copy(todir: spec.destinationDir) {
+            fileset(dir: scalacDestinationDir)
+        }
+        return result
+    }
+}
---
+++ src/main/groovy/jp/leafytree/android/AndroidScalaPlugin.groovy
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.leafytree.android
+import org.gradle.api.GradleException
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.file.SourceDirectorySet
+import org.gradle.api.internal.file.DefaultSourceDirectorySet
+import org.gradle.api.internal.file.FileResolver
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.util.ConfigureUtil
+
+import javax.inject.Inject
+
+public class AndroidScalaPlugin implements Plugin<Project> {
+    private final FileResolver fileResolver
+    private final Map<String, SourceDirectorySet> sourceDirectorySetMap = new HashMap<>()
+
+    @Inject
+    public AndroidScalaPlugin(FileResolver fileResolver) {
+        this.fileResolver = fileResolver
+    }
+
+    public void apply(Project project) {
+        project.extensions.create("androidScala", AndroidScalaPluginExtension)
+        def androidExtension = project.extensions.getByName("android")
+        updateAndroidSourceSetsExtension(project, androidExtension)
+        project.gradle.taskGraph.whenReady { taskGraph ->
+            addScalaDependencies(project)
+            taskGraph.allTasks.each { Task task ->
+                updateAndroidJavaCompileTask(project, androidExtension, task)
+            }
+        }
+    }
+
+    void addScalaDependencies(Project project) {
+        def scalaLibraryDependency = project.configurations.compile.allDependencies.find {
+            it.group == 'org.scala-lang' && it.name == "scala-library"
+        }
+        if (!scalaLibraryDependency) {
+            throw new GradleException("Dependency `compile \"org.scala-lang:scala-library:\$version\"' is not defined")
+        }
+        def version = scalaLibraryDependency.version
+        project.configurations { scalaCompileProvided }
+        project.dependencies.add("scalaCompileProvided", "org.scala-lang:scala-compiler:$version")
+    }
+
+    void updateAndroidSourceSetsExtension(Project project, Object androidExtension) {
+        ["main", "instrumentTest"].each { sourceSetName ->
+            def defaultSrcDir = ["src", sourceSetName, "scala"].join(File.separator)
+            def sourceSet = androidExtension.sourceSets."$sourceSetName"
+            def scala = new DefaultSourceDirectorySet("$sourceSet.name (scala)", fileResolver)
+            sourceDirectorySetMap[sourceSetName] = scala
+            scala.srcDir(defaultSrcDir)
+            scala.getFilter().include("**/*.scala");
+            if (sourceSet.metaClass.getMetaMethod("scala")) {
+                throw new GradleException('android.sourceSet.$type.scala already exists')
+            }
+            sourceSet.metaClass.getScala = { sourceDirectorySetMap[sourceSetName] }
+            sourceSet.metaClass.setScala = { it -> sourceDirectorySetMap[sourceSetName] = it }
+            sourceSet.metaClass.scala = { configureClosure ->
+                ConfigureUtil.configure(configureClosure, sourceDirectorySetMap[sourceSetName])
+                sourceSet.java.srcDirs = sourceSet.java.srcDirs + sourceDirectorySetMap[sourceSetName] // TODO: More clean code
+                androidExtension.sourceSets
+            }
+            sourceSet.java.srcDir(defaultSrcDir) // for Android Studio
+        }
+    }
+
+    void updateAndroidJavaCompileTask(Project project, Object androidExtension, Task task) {
+        if (!(task.name ==~ /^compile(.+)Java$/)) { // TODO: Use !=~ operator
+            return
+        }
+        if (!(task instanceof JavaCompile)) {
+            throw new GradleException("\"$task\" matches /^compile(.+)Java\$/ but is not instance of JavaCompile")
+        }
+        def test = (task.name ==~ /.+TestJava$/)
+        def key = test ? "instrumentTest" : "main" // TODO: Use /TestJava$/ regexp
+        task.source = task.source + sourceDirectorySetMap[key]
+        def options = [ target: project.androidScala.target ]
+        task.javaCompiler = new AndroidScalaJavaJointCompiler(project, task.javaCompiler, test, options)
+    }
+}

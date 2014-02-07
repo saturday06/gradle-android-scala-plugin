2488534fa42feb1c027e122ab2a078871b97b896
Isamu Mogi <saturday6c@gmail.com>
2014-02-07 12:25:58 +0900

    Use simple Activity

--- sample/libproject/app/src/main/java/jp/leafytree/android/libproject/AppActivity.java
+++ sample/libproject/app/src/main/java/jp/leafytree/android/libproject/AppActivity.java
@@ -1,12 +1,12 @@
 package jp.leafytree.android.libproject;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
 
 import jp.leafytree.android.libproject.lib1.Lib1Java;
 
-public class AppActivity extends ActionBarActivity {
+public class AppActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

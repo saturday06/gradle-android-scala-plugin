055aec801a4c3629224d5e76953dc4c1ef2f6782
Isamu Mogi <saturday6c@gmail.com>
2014-02-08 20:13:43 +0900

    Divide properties file generation

--- buildSrc/src/main/groovy/jp/leafytree/android/AndroidScalaPluginEndToEndTestTask.groovy
+++ buildSrc/src/main/groovy/jp/leafytree/android/AndroidScalaPluginEndToEndTestTask.groovy
@@ -16,18 +16,16 @@ public class AndroidScalaPluginEndToEndTestTask extends DefaultTask {
                     ["1.10", "2.10.3", "0.8.1", "19", "19.0.1", "4", "19"],
                     ["1.10", "2.10.3", "0.8.1", "19", "19.0.1", "7", "19"],
             ].each { buildArgs ->
-                runProject(* (projectArgs + buildArgs))
+                def gradleVersion = buildArgs.first()
+                def gradleWrapperProperties = getGradleWrapperProperties(gradleVersion)
+                def gradleProperties = getGradleProperties(buildArgs.drop(1))
+                println "Test $projectArgs gradleVersion:$gradleVersion $gradleProperties"
+                runProject(projectArgs[0], projectArgs[1], gradleWrapperProperties, gradleProperties)
             }
         }
     }
 
-    def runProject(projectName, tasks, gradleVersion, scalaLibraryVersion, androidPluginVersion, androidPluginCompileSdkVersion,
-                   androidPluginBuildToolsVersion, androidPluginMinSdkVersion, androidPluginTargetSdkVersion) {
-        // def snaphotRepositoryUrl = "http://saturday06.github.io/gradle-android-scala-plugin/repository/snapshot"
-        def snaphotRepositoryUrl = [project.buildFile.parentFile.absolutePath, "gh-pages", "repository", "snapshot"].join(File.separator)
-        def baseDir = new File([project.buildFile.parentFile.absolutePath, "src", "endToEndTest"].join(File.separator))
-        def projectDir = new File([baseDir.absolutePath, "project", projectName].join(File.separator))
-
+    def getGradleWrapperProperties(gradleVersion) {
         def gradleWrapperProperties = new Properties()
         gradleWrapperProperties.putAll([
                 distributionBase: "GRADLE_USER_HOME",
@@ -36,10 +34,13 @@ public class AndroidScalaPluginEndToEndTestTask extends DefaultTask {
                 zipStorePath: "wrapper/dists",
                 distributionUrl: "http://services.gradle.org/distributions/gradle-" + gradleVersion + "-bin.zip",
         ])
-        new File(baseDir, ["gradle", "wrapper", "gradle-wrapper.properties"].join(File.separator)).withWriter {
-            gradleWrapperProperties.store(it, "")
-        }
+        gradleWrapperProperties
+    }
 
+    def getGradleProperties(scalaLibraryVersion, androidPluginVersion, androidPluginCompileSdkVersion,
+                            androidPluginBuildToolsVersion, androidPluginMinSdkVersion, androidPluginTargetSdkVersion) {
+        // def snaphotRepositoryUrl = "http://saturday06.github.io/gradle-android-scala-plugin/repository/snapshot"
+        def snaphotRepositoryUrl = [project.buildFile.parentFile.absolutePath, "gh-pages", "repository", "snapshot"].join(File.separator)
         def gradleProperties = new Properties()
         gradleProperties.putAll([
                 "org.gradle.jvmargs": "-Xmx2048m -XX:MaxPermSize=1024m -XX:+HeapDumpOnOutOfMemoryError",
@@ -53,21 +54,23 @@ public class AndroidScalaPluginEndToEndTestTask extends DefaultTask {
                 androidPluginMinSdkVersion: androidPluginMinSdkVersion,
                 androidPluginTargetSdkVersion: androidPluginTargetSdkVersion,
         ])
+        gradleProperties
+    }
+
+    def runProject(projectName, tasks, gradleWrapperProperties, gradleProperties) {
+        def baseDir = new File([project.buildFile.parentFile.absolutePath, "src", "endToEndTest"].join(File.separator))
+        def projectDir = new File([baseDir.absolutePath, "project", projectName].join(File.separator))
+        new File(baseDir, ["gradle", "wrapper", "gradle-wrapper.properties"].join(File.separator)).withWriter {
+            gradleWrapperProperties.store(it, getClass().getName())
+        }
         new File(projectDir, "gradle.properties").withWriter {
-            gradleProperties.store(it, "")
+            gradleProperties.store(it, getClass().getName())
         }
 
-        def command = []
-        if (Os.isFamily(Os.FAMILY_WINDOWS)) {
-            command << new File(baseDir, "gradlew.bat").absolutePath
-        } else {
-            command << "/bin/sh"
-            command << new File(baseDir, "gradlew").absolutePath
-        }
-        command << "--stacktrace"
-        command << "--project-dir"
-        command << projectDir.absolutePath
-        command += tasks
+        def command = Os.isFamily(Os.FAMILY_WINDOWS) ?
+                [new File(baseDir, "gradlew.bat").absolutePath] :
+                ["/bin/sh", new File(baseDir, "gradlew").absolutePath]
+        command += ["--stacktrace", "--project-dir", projectDir.absolutePath] + tasks
         def stdout = new StringBuilder()
         def stderr = new StringBuilder()
         def process = command.execute()

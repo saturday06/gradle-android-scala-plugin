d41222c794214d297e42a15e0d7984078747f82e
Isamu Mogi <saturday6c@gmail.com>
2014-02-08 17:52:35 +0900

    Use Properties instead of here document

--- buildSrc/src/main/groovy/jp/leafytree/android/AndroidScalaPluginEndToEndTestTask.groovy
+++ buildSrc/src/main/groovy/jp/leafytree/android/AndroidScalaPluginEndToEndTestTask.groovy
@@ -1,8 +1,8 @@
 package jp.leafytree.android
 
+import org.apache.tools.ant.taskdefs.condition.Os
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
-import org.apache.tools.ant.taskdefs.condition.Os
 
 public class AndroidScalaPluginEndToEndTestTask extends DefaultTask {
     @TaskAction
@@ -13,8 +13,8 @@ public class AndroidScalaPluginEndToEndTestTask extends DefaultTask {
                 ["appAndLib", ["installDebug", "connectedInstrumentTest"]],
         ].each { projectArgs ->
             [
-                    ["1.10", "2.10.3", "0.8.1", 19, "19.0.1", 4, 19],
-                    ["1.10", "2.10.3", "0.8.1", 19, "19.0.1", 7, 19],
+                    ["1.10", "2.10.3", "0.8.1", "19", "19.0.1", "4", "19"],
+                    ["1.10", "2.10.3", "0.8.1", "19", "19.0.1", "7", "19"],
             ].each { buildArgs ->
                 runProject(* (projectArgs + buildArgs))
             }
@@ -25,33 +25,38 @@ public class AndroidScalaPluginEndToEndTestTask extends DefaultTask {
                    androidPluginBuildToolsVersion, androidPluginMinSdkVersion, androidPluginTargetSdkVersion) {
         // def snaphotRepositoryUrl = "http://saturday06.github.io/gradle-android-scala-plugin/repository/snapshot"
         def snaphotRepositoryUrl = [project.buildFile.parentFile.absolutePath, "gh-pages", "repository", "snapshot"].join(File.separator)
-        snaphotRepositoryUrl = snaphotRepositoryUrl.replaceAll('\\\\', '\\\\\\\\')
         def baseDir = new File([project.buildFile.parentFile.absolutePath, "src", "endToEndTest"].join(File.separator))
         def projectDir = new File([baseDir.absolutePath, "project", projectName].join(File.separator))
+
+        def gradleWrapperProperties = new Properties()
+        gradleWrapperProperties.putAll([
+                distributionBase: "GRADLE_USER_HOME",
+                distributionPath: "wrapper/dists",
+                zipStoreBase: "GRADLE_USER_HOME",
+                zipStorePath: "wrapper/dists",
+                distributionUrl: "http://services.gradle.org/distributions/gradle-" + gradleVersion + "-bin.zip",
+        ])
         new File(baseDir, ["gradle", "wrapper", "gradle-wrapper.properties"].join(File.separator)).withWriter {
-            it.write("""
-distributionBase=GRADLE_USER_HOME
-distributionPath=wrapper/dists
-zipStoreBase=GRADLE_USER_HOME
-zipStorePath=wrapper/dists
-distributionUrl=http\\://services.gradle.org/distributions/gradle-$gradleVersion-bin.zip
-""")
+            gradleWrapperProperties.store(it, "")
         }
 
+        def gradleProperties = new Properties()
+        gradleProperties.putAll([
+                "org.gradle.jvmargs": "-Xmx2048m -XX:MaxPermSize=1024m -XX:+HeapDumpOnOutOfMemoryError",
+                snaphotRepositoryUrl: snaphotRepositoryUrl,
+                scalaLibraryVersion: scalaLibraryVersion,
+                androidScalaTarget: "jvm-1.6",
+                androidScalaPluginVersion: "1.0-SNAPSHOT",
+                androidPluginVersion: androidPluginVersion,
+                androidPluginCompileSdkVersion: androidPluginCompileSdkVersion,
+                androidPluginBuildToolsVersion: androidPluginBuildToolsVersion,
+                androidPluginMinSdkVersion: androidPluginMinSdkVersion,
+                androidPluginTargetSdkVersion: androidPluginTargetSdkVersion,
+        ])
         new File(projectDir, "gradle.properties").withWriter {
-            it.write("""
-org.gradle.jvmargs=-Xmx2048m -XX:MaxPermSize=1024m -XX:+HeapDumpOnOutOfMemoryError
-snaphotRepositoryUrl=$snaphotRepositoryUrl
-scalaLibraryVersion=$scalaLibraryVersion
-androidScalaTarget=jvm-1.6
-androidScalaPluginVersion=1.0-SNAPSHOT
-androidPluginVersion=$androidPluginVersion
-androidPluginCompileSdkVersion=$androidPluginCompileSdkVersion
-androidPluginBuildToolsVersion=$androidPluginBuildToolsVersion
-androidPluginMinSdkVersion=$androidPluginMinSdkVersion
-androidPluginTargetSdkVersion=$androidPluginTargetSdkVersion
-""")
+            gradleProperties.store(it, "")
         }
+
         def command = []
         if (Os.isFamily(Os.FAMILY_WINDOWS)) {
             command << new File(baseDir, "gradlew.bat").absolutePath

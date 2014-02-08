c6a9f21259999babad7db0cf663c4fac0bb3f8f1
Isamu Mogi <saturday6c@gmail.com>
2014-02-08 17:26:18 +0900

    Divide test argument generation

--- buildSrc/src/main/groovy/jp/leafytree/android/AndroidScalaPluginEndToEndTestTask.groovy
+++ buildSrc/src/main/groovy/jp/leafytree/android/AndroidScalaPluginEndToEndTestTask.groovy
@@ -1,42 +1,45 @@
 package jp.leafytree.android
 
-import org.apache.tools.ant.taskdefs.condition.Os
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+import org.apache.tools.ant.taskdefs.condition.Os
 
 public class AndroidScalaPluginEndToEndTestTask extends DefaultTask {
     @TaskAction
     def run() {
-        println "Start EndToEndTest"
+        [
+                ["app", ["installDebug", "connectedInstrumentTest"]],
+                ["lib", ["connectedInstrumentTest"]],
+                ["appAndLib", ["installDebug", "connectedInstrumentTest"]],
+        ].each { projectArgs ->
+            [
+                    ["1.10", "2.10.3", "0.8.1", 19, "19.0.1", 4, 19],
+                    ["1.10", "2.10.3", "0.8.1", 19, "19.0.1", 7, 19],
+            ].each { buildArgs ->
+                runProject(* (projectArgs + buildArgs))
+            }
+        }
+    }
+
+    def runProject(projectName, tasks, gradleVersion, scalaLibraryVersion, androidPluginVersion, androidPluginCompileSdkVersion,
+                   androidPluginBuildToolsVersion, androidPluginMinSdkVersion, androidPluginTargetSdkVersion) {
         // def snaphotRepositoryUrl = "http://saturday06.github.io/gradle-android-scala-plugin/repository/snapshot"
         def snaphotRepositoryUrl = [project.buildFile.parentFile.absolutePath, "gh-pages", "repository", "snapshot"].join(File.separator)
         snaphotRepositoryUrl = snaphotRepositoryUrl.replaceAll('\\\\', '\\\\\\\\')
         def baseDir = new File([project.buildFile.parentFile.absolutePath, "src", "endToEndTest"].join(File.separator))
-        [
-                ["1.10", "2.10.3", "0.8.1", 19, "19.0.1", 4, 19],
-                ["1.10", "2.10.3", "0.8.1", 19, "19.0.1", 7, 19],
-        ].each {
-            println "Configuration: $it"
-            def (gradleVersion, scalaLibraryVersion, androidPluginVersion, androidPluginCompileSdkVersion,
-                 androidPluginBuildToolsVersion, androidPluginMinSdkVersion, androidPluginTargetSdkVersion) = it
-            new File(baseDir, ["gradle", "wrapper", "gradle-wrapper.properties"].join(File.separator)).withWriter {
-                it.write("""
+        def projectDir = new File([baseDir.absolutePath, "project", projectName].join(File.separator))
+        new File(baseDir, ["gradle", "wrapper", "gradle-wrapper.properties"].join(File.separator)).withWriter {
+            it.write("""
 distributionBase=GRADLE_USER_HOME
 distributionPath=wrapper/dists
 zipStoreBase=GRADLE_USER_HOME
 zipStorePath=wrapper/dists
 distributionUrl=http\\://services.gradle.org/distributions/gradle-$gradleVersion-bin.zip
 """)
-            }
-            [
-                    ["app", ["installDebug", "connectedInstrumentTest"]],
-                    ["lib", ["connectedInstrumentTest"]],
-                    ["appAndLib", ["installDebug", "connectedInstrumentTest"]],
-            ].each {
-                def (projectName, tasks) = it
-                def projectDir = new File([baseDir.absolutePath, "project", projectName].join(File.separator))
-                new File(projectDir, "gradle.properties").withWriter {
-                    it.write("""
+        }
+
+        new File(projectDir, "gradle.properties").withWriter {
+            it.write("""
 org.gradle.jvmargs=-Xmx2048m -XX:MaxPermSize=1024m -XX:+HeapDumpOnOutOfMemoryError
 snaphotRepositoryUrl=$snaphotRepositoryUrl
 scalaLibraryVersion=$scalaLibraryVersion
@@ -48,23 +51,23 @@ androidPluginBuildToolsVersion=$androidPluginBuildToolsVersion
 androidPluginMinSdkVersion=$androidPluginMinSdkVersion
 androidPluginTargetSdkVersion=$androidPluginTargetSdkVersion
 """)
-                }
-                def command = []
-                if (org.apache.tools.ant.taskdefs.condition.Os.isFamily(Os.FAMILY_WINDOWS)) {
-                    command << new File(baseDir, "gradlew.bat").absolutePath
-                } else {
-                    command << "/bin/sh"
-                    command << new File(baseDir, "gradlew").absolutePath
-                }
-                command << "--stacktrace"
-                command << "--project-dir"
-                command << projectDir.absolutePath
-                command += tasks
-                def stdout = new StringBuilder()
-                def stderr = new StringBuilder()
-                def process = command.execute()
-                process.waitForProcessOutput(stdout, stderr)
-                println("""
+        }
+        def command = []
+        if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+            command << new File(baseDir, "gradlew.bat").absolutePath
+        } else {
+            command << "/bin/sh"
+            command << new File(baseDir, "gradlew").absolutePath
+        }
+        command << "--stacktrace"
+        command << "--project-dir"
+        command << projectDir.absolutePath
+        command += tasks
+        def stdout = new StringBuilder()
+        def stderr = new StringBuilder()
+        def process = command.execute()
+        process.waitForProcessOutput(stdout, stderr)
+        println("""
 -- command --
 $command
 -- stdout ---
@@ -72,10 +75,8 @@ $stdout
 -- stderr ---
 $stderr
 """)
-                if (process.exitValue() != 0) {
-                    throw new IOException("process.exitValue != 0 but ${process.exitValue()}")
-                }
-            }
+        if (process.exitValue() != 0) {
+            throw new IOException("process.exitValue != 0 but ${process.exitValue()}")
         }
     }
 }

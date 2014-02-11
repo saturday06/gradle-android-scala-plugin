309337c01e2d22b10e8c90120d27c30ca82548a3
Isamu Mogi <saturday6c@gmail.com>
2014-02-11 23:29:39 +0900

    Change plugin classpath to jp.leafytree.gradle

--- src/main/groovy/jp/leafytree/gradle/AndroidScalaPluginExtension.groovy
+++ src/main/groovy/jp/leafytree/gradle/AndroidScalaPluginExtension.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package jp.leafytree.android
+package jp.leafytree.gradle
 
 class AndroidScalaPluginExtension {
     String target = "jvm-1.6"

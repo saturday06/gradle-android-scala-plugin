05403a8e065c31eac725ed64006b3869f225636e
Isamu Mogi <saturday6c@gmail.com>
2014-02-14 21:01:16 +0900

    Update android plugin to 0.8.3

--- buildSrc/src/main/groovy/jp/leafytree/gradle/AndroidScalaPluginIntegrationTestTask.groovy
+++ buildSrc/src/main/groovy/jp/leafytree/gradle/AndroidScalaPluginIntegrationTestTask.groovy
@@ -27,8 +27,8 @@ public class AndroidScalaPluginIntegrationTestTask extends DefaultTask {
                 ["appAndLib", ["installDebug", "connectedInstrumentTest"]],
         ].each { projectArgs ->
             [
-                    ["1.10", "2.10.3", "0.8.1", "19", "19.0.1", "4", "19"],
-                    ["1.10", "2.11.0-M8", "0.8.1", "19", "19.0.1", "4", "19"],
+                    ["1.10", "2.10.3", "0.8.3", "19", "19.0.1", "4", "19"],
+                    ["1.10", "2.11.0-M8", "0.8.3", "19", "19.0.1", "4", "19"],
             ].each { buildArgs ->
                 def gradleVersion = buildArgs.first()
                 def gradleWrapperProperties = getGradleWrapperProperties(gradleVersion)

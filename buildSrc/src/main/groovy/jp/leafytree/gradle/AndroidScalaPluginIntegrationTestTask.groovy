27adb6013dbd7a6390ca8879cc486eb66493b4cc
Isamu Mogi <saturday6c@gmail.com>
2014-04-08 23:37:34 +0900

    Add clean test projects for integrationTest and sample task

--- buildSrc/src/main/groovy/jp/leafytree/gradle/AndroidScalaPluginIntegrationTestTask.groovy
+++ buildSrc/src/main/groovy/jp/leafytree/gradle/AndroidScalaPluginIntegrationTestTask.groovy
@@ -28,11 +28,12 @@ public class AndroidScalaPluginIntegrationTestTask extends DefaultTask {
                 ["lib", ["connectedAndroidTest"], false],
                 ["appAndLib", ["installDebug", "connectedAndroidTest"], true],
         ].findAll { x, y, runOnTravis -> (!travis || runOnTravis) }.each { projectName, gradleArgs, runOnTravis ->
+            gradleArgs = ["clean", *gradleArgs, "uninstallAll"]
             [
                     ["1.11", "2.10.4", "0.9.2", "19", "19.0.3", "4", "19"],
                     ["1.11", "2.11.0-RC4", "0.9.2", "19", "19.0.3", "4", "19"],
             ].each { testParameters ->
-                gradleArgs.add("uninstallAll")
+                gradleArgs = ["clean", *gradleArgs, "uninstallAll"]
                 def gradleVersion = testParameters.first()
                 def gradleWrapperProperties = getGradleWrapperProperties(gradleVersion)
                 def gradleProperties = getGradleProperties(testParameters.drop(1))

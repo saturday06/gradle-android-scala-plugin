542883823bb7cf0932fb2d9d3746c0c975bc05b1
Isamu Mogi <saturday6c@gmail.com>
2014-04-14 00:16:53 +0900

    Use scala runtime library in integration test

--- src/integTest/project/useScalaOnlyTest/app/src/androidTest/scala/jp/leafytree/android/libproject/app/Lib1JavaTest.scala
+++ src/integTest/project/useScalaOnlyTest/app/src/androidTest/scala/jp/leafytree/android/libproject/app/Lib1JavaTest.scala
@@ -6,5 +6,6 @@ import jp.leafytree.android.libproject.lib1.Lib1Java
 class Lib1JavaTest extends TestCase {
   def test1() {
     Assert.assertEquals("Lib1Java", new Lib1Java().getName)
+    Assert.assertEquals(List("Lib1Java").last, Option(new Lib1Java().getName).get)
   }
 }
--- src/integTest/project/useScalaOnlyTest/lib/src/androidTest/scala/jp/leafytree/android/libproject/lib1/Lib1JavaTest.scala
+++ src/integTest/project/useScalaOnlyTest/lib/src/androidTest/scala/jp/leafytree/android/libproject/lib1/Lib1JavaTest.scala
@@ -5,5 +5,6 @@ import junit.framework.{TestCase, Assert}
 class Lib1JavaTest extends TestCase {
   def test1() {
     Assert.assertEquals("Lib1Java", new Lib1Java().getName)
+    Assert.assertEquals(List("Lib1Java").last, Option(new Lib1Java().getName).get)
   }
 }

e09eb94d9d53d4a66c200b28a696cfaa96bfd299
Isamu Mogi <isamu@leafytree.jp>
2014-10-31 21:25:21 +0900

    Add test for changed srcDirs

---
+++ src/integTest/project/simpleFlavor/src/flavor3/java/jp/leafytree/android/hello/FlavorJava.java
@@ -0,0 +1,9 @@
+package jp.leafytree.android.hello;
+
+import junit.framework.Assert;
+
+public class FlavorJava {
+    public String name() {
+        Assert.fail("flavor3 moved!");
+    }
+}
---
+++ src/integTest/project/simpleFlavor/src/flavor3/scala/jp/leafytree/android/hello/FlavorScala.scala
@@ -0,0 +1,9 @@
+package jp.leafytree.android.hello
+
+import junit.framework.Assert
+
+class FlavorScala {
+  def name() = {
+    Assert.fail("flavor3 moved!")
+  }
+}

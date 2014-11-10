36f9c926a91234dd378a584eebcc902b38259b88
Isamu Mogi <isamu@leafytree.jp>
2014-11-10 20:42:53 +0900

    Prepare for next development iteration

--- buildSrc/src/main/groovy/jp/leafytree/gradle/AndroidScalaPluginIntegrationTestTask.groovy
+++ buildSrc/src/main/groovy/jp/leafytree/gradle/AndroidScalaPluginIntegrationTestTask.groovy
@@ -71,7 +71,7 @@ public class AndroidScalaPluginIntegrationTestTask extends DefaultTask {
                 snaphotRepositoryUrl: snaphotRepositoryUrl,
                 scalaLibraryVersion: scalaLibraryVersion,
                 scalaDependencyVersion: scalaLibraryVersion.split("\\.").take(2).join("."),
-                androidScalaPluginVersion: "1.3-SNAPSHOT",
+                androidScalaPluginVersion: "1.3.1-SNAPSHOT",
                 androidPluginVersion: androidPluginVersion,
                 androidPluginCompileSdkVersion: androidPluginCompileSdkVersion,
                 androidPluginBuildToolsVersion: androidPluginBuildToolsVersion,

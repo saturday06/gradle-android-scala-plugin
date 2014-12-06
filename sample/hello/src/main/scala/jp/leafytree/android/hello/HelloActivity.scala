c89e362402e2a2ee4bb4f6a59a5fa7524fa088e8
Isamu Mogi <isamu@leafytree.jp>
2014-12-06 14:03:14 +0900

    Add big jars to break 64k limit

--- sample/hello/src/main/scala/jp/leafytree/android/hello/HelloActivity.scala
+++ sample/hello/src/main/scala/jp/leafytree/android/hello/HelloActivity.scala
@@ -2,7 +2,11 @@ package jp.leafytree.android.hello
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import com.google.common.collect.ImmutableSet
+import org.apache.commons.math3.analysis.function.Abs
+import scalaz.Scalaz._
 
 class HelloActivity extends Activity {
   override def onCreate(savedInstanceState: Bundle) {
@@ -10,5 +14,11 @@ class HelloActivity extends Activity {
     setContentView(R.layout.activity_hello)
     val scalaTextView = findViewById(R.id.scala_text_view).asInstanceOf[TextView]
     scalaTextView.setText(new HelloJava().say())
+
+    val values = for {
+      str <- List("1", "2", "3", "string", "5")
+      int <- str.parseInt.toOption
+    } yield (new Abs()).value(int)
+    Log.d("debug", "" + ImmutableSet.of(values))
   }
 }
--- sample/libproject/app/src/main/scala/jp/leafytree/android/libproject/AppActivity.scala
+++ sample/libproject/app/src/main/scala/jp/leafytree/android/libproject/AppActivity.scala
@@ -2,8 +2,12 @@ package jp.leafytree.android.libproject
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import com.google.common.collect.ImmutableSet
+import scalaz.Scalaz._
 import jp.leafytree.android.libproject.lib1.Lib1Java
+import org.apache.commons.math3.analysis.function.Abs
 
 class AppActivity extends Activity {
     override def onCreate(savedInstanceState: Bundle) {
@@ -11,5 +15,11 @@ class AppActivity extends Activity {
         setContentView(R.layout.activity_app)
         val messageTextView = findViewById(R.id.message_text_view).asInstanceOf[TextView]
         messageTextView.setText(new Lib1Java().getName())
+
+        val values = for {
+            str <- List("1", "2", "3", "string", "5")
+            int <- str.parseInt.toOption
+        } yield (new Abs()).value(int)
+        Log.d("debug", "" + ImmutableSet.of(values))
     }
 }

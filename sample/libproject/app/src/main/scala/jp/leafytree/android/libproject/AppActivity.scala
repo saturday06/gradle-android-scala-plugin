60ab97dbe444d3335e2ca8e49a142ed7254d1f2f
Isamu Mogi <isamu@leafytree.jp>
2014-12-06 16:58:27 +0900

    Indent

--- sample/libproject/app/src/main/scala/jp/leafytree/android/libproject/AppActivity.scala
+++ sample/libproject/app/src/main/scala/jp/leafytree/android/libproject/AppActivity.scala
@@ -10,16 +10,16 @@ import jp.leafytree.android.libproject.lib1.Lib1Java
 import org.apache.commons.math3.analysis.function.Abs
 
 class AppActivity extends Activity {
-    override def onCreate(savedInstanceState: Bundle) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_app)
-        val messageTextView = findViewById(R.id.message_text_view).asInstanceOf[TextView]
-        messageTextView.setText(new Lib1Java().getName())
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_app)
+    val messageTextView = findViewById(R.id.message_text_view).asInstanceOf[TextView]
+    messageTextView.setText(new Lib1Java().getName())
 
-        val values = for {
-            str <- List("1", "2", "3", "string", "5")
-            int <- str.parseInt.toOption
-        } yield (new Abs()).value(int)
-        Log.d("debug", "" + ImmutableSet.of(values))
-    }
+    val values = for {
+      str <- List("1", "2", "-3", "string", "5")
+      int <- str.parseInt.toOption
+    } yield (new Abs()).value(int)
+    Log.d("debug", "" + ImmutableSet.of(values))
+  }
 }

9fb460aefe99d7214812bce3a5b44c9cc09d6077
Isamu Mogi <isamu@leafytree.jp>
2016-01-01 22:20:52 +0900

    getBootClasspath(false)

--- src/main/groovy/jp/leafytree/gradle/AndroidScalaPlugin.groovy
+++ src/main/groovy/jp/leafytree/gradle/AndroidScalaPlugin.groovy
@@ -213,7 +213,7 @@ public class AndroidScalaPlugin implements Plugin<Project> {
         scalaCompileTask.sourceCompatibility = javaCompileTask.sourceCompatibility
         scalaCompileTask.targetCompatibility = javaCompileTask.targetCompatibility
         scalaCompileTask.scalaCompileOptions.encoding = javaCompileTask.options.encoding
-        scalaCompileTask.classpath = javaCompileTask.classpath + project.files(androidPlugin.androidBuilder.bootClasspath)
+        scalaCompileTask.classpath = javaCompileTask.classpath + project.files(androidPlugin.androidBuilder.getBootClasspath(false))
         scalaCompileTask.scalaClasspath = compilerConfiguration.asFileTree
         scalaCompileTask.zincClasspath = zincConfiguration.asFileTree
         scalaCompileTask.scalaCompileOptions.incrementalOptions.analysisFile = new File(variantWorkDir, "analysis.txt")

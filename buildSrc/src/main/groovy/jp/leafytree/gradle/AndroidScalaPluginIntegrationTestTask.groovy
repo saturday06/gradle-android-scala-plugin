154cf3ed0b32c659a784b2c4a63eca61282c712c
deanrobertcook <deanrobertcook@gmail.com>
2016-07-20 15:49:57 +0200

    Upgrade tested android gradle pluging to 2.1.0

--- buildSrc/src/main/groovy/jp/leafytree/gradle/AndroidScalaPluginIntegrationTestTask.groovy
+++ buildSrc/src/main/groovy/jp/leafytree/gradle/AndroidScalaPluginIntegrationTestTask.groovy
@@ -22,7 +22,7 @@ import org.gradle.api.tasks.TaskAction
 public class AndroidScalaPluginIntegrationTestTask extends DefaultTask {
 
     static def GRADLE_VERSION = "2.12"
-    static def ANDROID_GRADLE_PLUGIN_VERSION = "1.5.0"
+    static def ANDROID_GRADLE_PLUGIN_VERSION = "2.1.0"
     static def ANDROID_BUILD_TOOLS_VERSION = "23.0.3"
 
     @TaskAction
